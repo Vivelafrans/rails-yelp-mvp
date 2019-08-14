1cc14ee5d2b9e74b761b6c020777e39526821463
Frans Knaapen <fransknaapen@gmail.com>
2019-08-14 17:57:10 +0200

    still missing the reviews part and a lot of styling...

--- Gemfile
+++ Gemfile
@@ -27,6 +27,11 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+# Added gem files from Le Wagon rails-stylesheets
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Clearing database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Hotel de Goudfazant',
+    address: 'Aambeeldstraat 34',
+    category: 'french',
+    phone_number: '+31633223801'
+  },
+  {
+    name: 'Scheepskameel',
+    address: 'Marineterrein 15',
+    category: 'italian',
+    phone_number: '+31633223801'
+  },
+  {
+    name: 'Cafe Modern',
+    address: 'Van der Pekstraat 54',
+    category: 'french',
+    phone_number: '+31633223801'
+  },
+  {
+    name: 'Rijssel',
+    address: 'Wibautstraat 45',
+    category: 'belgian',
+    phone_number: '+31633223801'
+  },
+  {
+    name: 'Fou Fow',
+    address: 'Ferdinand Bolstraat 24',
+    category: 'japanese',
+    phone_number: '+31633223801'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts "Finished! Populated the database with #{Restaurant.count} restaurants."
